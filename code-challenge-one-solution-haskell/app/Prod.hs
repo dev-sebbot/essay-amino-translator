937b4916af45a34b4a60c6082ecb4a035c09343a
Sebastian Benjamin <sebastiancbenjamin@gmail.com>
2019-08-01 23:51:13 -0700

    Revert to old Prod.hs

--- code-challenge-one-solution-haskell/app/Prod.hs
+++ code-challenge-one-solution-haskell/app/Prod.hs
@@ -1,42 +1,42 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Prod where
+import System.Environment
 import Control.Concurrent.Async
 import qualified Data.ByteString as B
 a [] = return ()
-a l  = do 
+a l  = do
  x <- head l
  a $ tail l
  w <- wait x
  return ()
-s i 
+s i
  | i == 50 = []
- | True = do [async $ k [(i * 15000) .. (i+1 * 15000)]] ++ (s $ i+1)
+ | True = do [async (k [i * 15000 .. (i+1) * 15000])] ++ (s $ i+1)
 k [] = do return ()
-k l  = do 
- B.writeFile (show $ head l) qu
+k l  = do
+ B.writeFile (show $ head l) (B.append q q)
  k $ tail l
-main = do 
+main = do
  a $ s 0
-qu = B.append q q
 q = "{-# LANGUAGE OverloadedStrings #-}\
 \module Prod where\
+\import System.Environment\
 \import Control.Concurrent.Async\
-\import Data.ByteString.UTF8 as V\
+\import Data.Char\
 \import qualified Data.ByteString as B\
 \a [] = return ()\
-\a l  = do \
+\a l  = do\
 \ x <- head l\
 \ a $ tail l\
 \ w <- wait x\
 \ return ()\
-\s i \
+\s i\
 \ | i == 50 = []\
-\ | True = do [async $ k [i * 15000 .. i+1 * 15000]] ++ (s $ i+1)\
+\ | True = do [async (k [i * 15000 .. (i+1) * 15000])] ++ (s $ i+1)\
 \k [] = do return ()\
-\k l  = do \
-\ B.writeFile (show $ head l) qu\
+\k l  = do\
+\ B.writeFile (show $ head l) (q ++ show q)\
 \ k $ tail l\
-\main = do \
+\main = do\
 \ a $ s 0\
-\qu = B.append q q\
-\q = " :: B.ByteString
+\q =" :: B.ByteString

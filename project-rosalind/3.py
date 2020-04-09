ad4219e49f5a0c4611109d90332ac8a3959bd267
Sebastian <sebastiancbenjamin@gmail.com>
2020-04-09 16:48:45 -0700

    Reverse complement

---
+++ project-rosalind/3.py
@@ -0,0 +1,21 @@
+#Given a DNA string t, return its reverse complement t^c
+
+import sys
+
+def complement(char):
+  if char == "A":
+    return "T"
+  if char == "T":
+    return "A"
+  if char == "C":
+    return "G"
+  if char == "G":
+    return "C"
+
+if __name__ == "__main__":
+  if len(sys.argv) != 2:
+    print("Usage:\n    python 3.py <filename>")
+    exit()
+
+  with open(sys.argv[1]) as f:
+    print(''.join(map(complement, f.readline()))[::-1])
